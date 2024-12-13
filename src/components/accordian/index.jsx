fcb52c2e4e69bc6ba17b382be0e77a0f147265a4
milanbasakhora <milanbasakhora@gmail.com>
2024-12-13 17:33:39 +0545

    accordion fixed

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -17,7 +17,7 @@ export default function Accordian() {
   function handleMultiSelection(getCurrentId) {
     let cpyMultiple = [...multiple];
     const findIndexOfCurrentId = cpyMultiple.indexOf(getCurrentId);
-    
+
     console.log(findIndexOfCurrentId);
 
     if (findIndexOfCurrentId === -1) cpyMultiple.push(getCurrentId);
@@ -47,10 +47,19 @@ export default function Accordian() {
                 <h3>{dataItem.question}</h3>
                 <span>+</span>
               </div>
-              {selected === dataItem.id ||
+
+              {enableMultiSelection
+                ? multiple.indexOf(dataItem.id) !== -1 && (
+                    <div className="content">{dataItem.answer}</div>
+                  )
+                : selected === dataItem.id && (
+                    <div className="content">{dataItem.answer}</div>
+                  )}
+
+              {/* {selected === dataItem.id ||
               multiple.indexOf(dataItem.id) !== 1 ? (
                 <div className="content">{dataItem.answer}</div>
-              ) : null}
+              ) : null} */}``
             </div>
           ))
         ) : (
